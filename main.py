9a512fe4c80d642b43dec63f61719b2708011402
mimmolamparelli <mimmo.lamparelli@gmail.com>
2023-04-25 17:03:56 +0200

    add button callback function

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import tkinter as tk
 # from tkinter import LabelFrame
 from tkinter import Button, Text, Label
 
+def addTimeLine():
+    print ("adding a timeline")
+    year = txtYear.get("1.0","end-1c")
+    event = txtEvent.get("1.0","end-1c")
+    print (f"year:{year} event:{event}")
+
 window = tk.Tk()
 window.title("Timelines")
 window.geometry("600x400")
 
+timeline = {}
 lblYear = Label(window, text="Year", font=("Verdana", 8))
 lblYear.pack()
 lblYear.place(x=1, y=1)
@@ -22,10 +29,10 @@ txtEvent = Text(window, font=("Verdana", 8), height=1, width=30)
 txtEvent.pack()
 txtEvent.place(x=80, y=18)
 
-btnAdd = Button(window, text="Add", height=1, width=10)
+btnAdd = Button(window, text="Add", height=1, width=5,command=addTimeLine)
 btnAdd.pack()
 btnAdd.place(x=400, y=1)
-# b1 = Button(window, text="One")
-# b1.pack()
-# b1.place(x=400, y=1)
+
+
+
 tk.mainloop()
