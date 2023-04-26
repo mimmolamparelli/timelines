b4334d78827544204a4d9ac51baba1b770ce7ac4
mimmolamparelli <mimmo.lamparelli@gmail.com>
2023-04-26 09:46:53 +0200

    Add TimeLine OK

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import tkinter as tk
 # from tkinter import LabelFrame
-from tkinter import Button, Text, Label
+from tkinter import Button, Text, Label, Listbox
+
+def SortTimeLine():
+    global timeline
+    tlKeys= list(timeline.keys())
+    tlKeys.sort()
+    timeline = {i: timeline[i] for i in tlKeys}
+    
 
 def addTimeLine():
-    print ("adding a timeline")
     year = txtYear.get("1.0","end-1c")
     event = txtEvent.get("1.0","end-1c")
     print (f"year:{year} event:{event}")
+    timeline[year]=event
+    SortTimeLine()
+    updateEventsList()
+
+def updateEventsList():
+    lstEvents.delete(0,tk.END)
+    counter = 0
+    for elem in timeline:
+        lstEvents.insert(counter,f"{elem}|{timeline[elem]}")
+        counter +=1
 
 window = tk.Tk()
 window.title("Timelines")
@@ -29,10 +45,16 @@ txtEvent = Text(window, font=("Verdana", 8), height=1, width=30)
 txtEvent.pack()
 txtEvent.place(x=80, y=18)
 
-btnAdd = Button(window, text="Add", height=1, width=5,command=addTimeLine)
+btnAdd = Button(window, text="Add", height=1, width=3,command=addTimeLine)
 btnAdd.pack()
 btnAdd.place(x=400, y=1)
 
+lstEvents = Listbox()
+lstEvents.pack()
+lstEvents.place(x=1,y=36,width=600,height=200)
 
+btnQuit = Button(window, text="Quit", height=1, width=3, command=quit)
+btnQuit.pack()
+btnQuit.place(x=500,y=1)
 
 tk.mainloop()
