8b86d2b706763ad8bab4fe6c1d0037ec5bbd2eb4
mimmolamparelli <mimmo.lamparelli@gmail.com>
2023-04-26 15:14:32 +0200

    added save file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
+import tkinter.filedialog as fd
+import json
 # from tkinter import LabelFrame
 from tkinter import Button, Text, Label, Listbox
+
 
 def SortTimeLine():
     global timeline
@@ -8,11 +11,10 @@ def SortTimeLine():
     tlKeys.sort()
     timeline = {i: timeline[i] for i in tlKeys}
     
-
 def addTimeLine():
-    year = txtYear.get("1.0","end-1c")
-    event = txtEvent.get("1.0","end-1c")
-    print (f"year:{year} event:{event}")
+    year = txtYear.get("1.0","end-1c").strip()
+    event = txtEvent.get("1.0","end-1c").strip()
+    # print (f"year:{year} event:{event}")
     timeline[year]=event
     SortTimeLine()
     updateEventsList()
@@ -24,11 +26,35 @@ def updateEventsList():
         lstEvents.insert(counter,f"{elem}|{timeline[elem]}")
         counter +=1
 
+def removeItem():
+    selectionList=[]
+    global timeline
+    sel = lstEvents.curselection()
+    for s in sel:
+        elem = lstEvents.get(s).split("|")[0]
+        del timeline[elem]
+    updateEventsList()
+    
+def loadTimeLine():
+    global timeline
+    f=fd.askopenfile().name
+    print (f"file name:{f}")
+
+def saveTimeLine():
+    global timeline
+    f=fd.asksaveasfile().name
+    print (f)
+    with open(f,"w") as outfile:
+        json_object = json.dump(timeline,outfile)
+
+#region Main 
 window = tk.Tk()
 window.title("Timelines")
 window.geometry("600x400")
 
-timeline = {}
+timeline={"1492":"Scoperta dell'America","1990":"GUNNS","1982":"Campioni del Mondo"}
+
+
 lblYear = Label(window, text="Year", font=("Verdana", 8))
 lblYear.pack()
 lblYear.place(x=1, y=1)
@@ -45,16 +71,30 @@ txtEvent = Text(window, font=("Verdana", 8), height=1, width=30)
 txtEvent.pack()
 txtEvent.place(x=80, y=18)
 
-btnAdd = Button(window, text="Add", height=1, width=3,command=addTimeLine)
+btnAdd = Button(window, text="Add", height=1, width=1,command=addTimeLine)
 btnAdd.pack()
 btnAdd.place(x=400, y=1)
 
-lstEvents = Listbox()
+lstEvents = Listbox(window,font=("Verdana,8"),selectmode=tk.MULTIPLE)
 lstEvents.pack()
 lstEvents.place(x=1,y=36,width=600,height=200)
 
-btnQuit = Button(window, text="Quit", height=1, width=3, command=quit)
+btnQuit = Button(window, text="Quit", height=1, width=1, command=quit)
 btnQuit.pack()
 btnQuit.place(x=500,y=1)
 
+btnDelete = Button(window, text="Delete", height=1, width=2, command=removeItem)
+btnDelete.pack()
+btnDelete.place(x=440,y=1)
+
+btnLoad = Button(window,text="Load",height=1,width=1,command=loadTimeLine)
+btnLoad.pack()
+btnLoad.place(x=360,y=1)
+
+btnSave = Button(window,text="save",height=1,width=1,command=saveTimeLine)
+btnSave.pack()
+btnSave.place(x=320,y=1)
+
+updateEventsList()
 tk.mainloop()
+#endregion
