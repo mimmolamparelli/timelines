a5a8d5cd9de142a0e8c48df4292a0bfe46a8e6af
mimmolamparelli <mimmo.lamparelli@gmail.com>
2023-04-27 09:17:42 +0200

    line

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import tkinter as tk
 import tkinter.filedialog as fd
 import json
 # from tkinter import LabelFrame
-from tkinter import Button, Text, Label, Listbox
-
+from tkinter import Button, Text, Label, Listbox, Canvas
+# from tkinter import Scrollbar
 
 def SortTimeLine():
     global timeline
@@ -50,6 +50,15 @@ def saveTimeLine():
     with open(f,"w") as outfile:
         json_object = json.dump(timeline,outfile)
 
+def drawTimeLine():
+    lenght = 570
+    start = 10
+    y = 30
+    w = 2
+    cnvGraph.create_line(start,y,lenght,y,fill="green",width=w)
+    
+
+
 #region Main 
 window = tk.Tk()
 window.title("Timelines")
@@ -57,6 +66,9 @@ window.geometry("600x400")
 
 # timeline={"1492":"Scoperta dell'America","1990":"GUNNS","1982":"Campioni del Mondo"}
 timeline = {}
+
+# scrollbar = Scrollbar(window)
+# scrollbar.pack(side=tk.RIGHT, fill="y")
 
 lblYear = Label(window, text="Year", font=("Verdana", 8))
 lblYear.pack()
@@ -76,27 +88,37 @@ txtEvent.place(x=80, y=18)
 
 btnAdd = Button(window, text="Add", height=1, width=1,command=addTimeLine)
 btnAdd.pack()
-btnAdd.place(x=400, y=1)
+btnAdd.place(x=440, y=1)
 
+# lstEvents = Listbox(window,font=("Verdana,8"),selectmode=tk.MULTIPLE,yscrollcommand = scrollbar.set)
 lstEvents = Listbox(window,font=("Verdana,8"),selectmode=tk.MULTIPLE)
 lstEvents.pack()
 lstEvents.place(x=1,y=36,width=600,height=200)
+# scrollbar.config(command=lstEvents.yview)
 
 btnQuit = Button(window, text="Quit", height=1, width=1, command=quit)
 btnQuit.pack()
-btnQuit.place(x=500,y=1)
+btnQuit.place(x=540,y=1)
 
 btnDelete = Button(window, text="Delete", height=1, width=2, command=removeItem)
 btnDelete.pack()
-btnDelete.place(x=440,y=1)
+btnDelete.place(x=480,y=1)
 
 btnLoad = Button(window,text="Load",height=1,width=1,command=loadTimeLine)
 btnLoad.pack()
-btnLoad.place(x=360,y=1)
+btnLoad.place(x=400,y=1)
 
 btnSave = Button(window,text="save",height=1,width=1,command=saveTimeLine)
 btnSave.pack()
-btnSave.place(x=320,y=1)
+btnSave.place(x=350,y=1)
+
+cnvGraph=Canvas(window,bg="light yellow",width=600)
+cnvGraph.pack()
+cnvGraph.place(x=1,y=250)
+
+btnTimeLine=Button(window,text="Timeline",width=3,height=1,command=drawTimeLine)
+btnTimeLine.pack()
+btnTimeLine.place(x=290,y=1)
 
 updateEventsList()
 tk.mainloop()
